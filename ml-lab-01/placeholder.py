791b98b6525156230b5493c9046cc7f9cfe1fb3a
eunho <76768140sa@gmail.com>
2025-07-22 14:24:34 +0900

    practice linear regression

--- ml-lab-01/placeholder.py
+++ ml-lab-01/placeholder.py
@@ -5,5 +5,5 @@ import tensorflow as tf
 def adder_node(a, b):
     return tf.add(a, b)
 
-print(adder_node(3, 4.5).numpy())         # 7.5
+print(adder_node(3.0, 4.5).numpy())         # 7.5
 print(adder_node([1.0, 3.0], [2.0, 4.0]).numpy())  # [3. 7.]
