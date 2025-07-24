fd6c508089b5ba6411015a7b4420129c4220b27f
eunho <76768140sa@gmail.com>
2025-07-24 22:20:42 +0900

    practice file input linear regression

--- ml-lab-04/file_input_linear_regression.py
+++ ml-lab-04/file_input_linear_regression.py
@@ -2,18 +2,16 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+import numpy as np
 
-x_data = [[73., 80., 75.],
-          [93., 88., 93.],
-          [89., 91., 90.],
-          [96., 98., 100.],
-          [73., 66., 70.]]
-y_data = [[152.],
-          [185.],
-          [180.],
-          [196.],
-          [142.]]
+# 절대경로로 변경
+xy = np.loadtxt('D:/workspace-python/DeepLearningZeroToAll/ml-lab-04/data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
 
+# Make sure the shape and data are OK
+print(x_data, "\nx_data shape:", x_data.shape)
+print(y_data, "\ny_data shape:", y_data.shape)
 
 # placeholders for a tensor that will be always fed.
 X = tf.placeholder(tf.float32, shape=[None, 3])
@@ -38,10 +36,10 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for step in range(2001):
-    cost_val, hy_val, _ = sess.run(
-        [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
+    cost_val, hy_val, _ = sess.run([cost, hypothesis, train], 
+                                   feed_dict={X: x_data, Y: y_data})
     if step % 10 == 0:
-        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+        print(step, "Cost:", cost_val, "\nPrediction:\n", hy_val)
 
 # Ask my score
 print("Your score will be ", sess.run(hypothesis, 
